d3ba9a85d677dae68e5a694d3df9aced27a000df
nwy140 <nwy140@gmail.com>
2017-09-14 09:47:24 +0800

    Update LoopGenerateUnlimitedInstance.bat

--- LoopGenerateUnlimitedInstance.bat
+++ LoopGenerateUnlimitedInstance.bat
@@ -4,8 +4,8 @@ msg Username You have been hacked,  a noob like you stands no chance against me
 
 REM No username
 msg * You have been hacked,  a noob like you stands no chance against me
-msg * cant believe you dont even know how to code \n
-msg * I don't think you are clever enough to run TASKKILL /im cmd.exe /f \n
+msg * cant believe you dont even know how to code 
+msg * I don't think you are clever enough to run TASKKILL /im cmd.exe /f 
 
 
 
