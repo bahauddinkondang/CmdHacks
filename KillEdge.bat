5759533d73f444d6502daaa27f2496fbe8164c93
nwy140 <nwy140@gmail.com>
2017-09-20 23:07:11 +0800

    Update KillEdge.bat

--- KillEdge.bat
+++ KillEdge.bat
@@ -1,6 +1,5 @@
 REM place vbs script in shell:startup , if you are somebody as noob as justin chin or people like jeremy lai ting hong, you wont get what  what I am saying
 REM Justin Chin plagiarised my coding style some time ago I think, but who cares. This is open source anyway
-REM  in reality being good in video games like dota/lol doesnt make you any better than a game developer ,
 
 :edges
 taskkill /im MicrosoftEdge.exe
@@ -9,3 +8,13 @@ taskkill /im MicrosoftEdgeCP.exe
 
 
 goto edges
+
+
+
+
+
+
+
+
+
+REM  in reality being good in video games like dota/lol doesnt make you any better than a game developer ,
