c245f78986c13f9b57e95c1e422f2ab76d7ab132
nwy140 <nwy140@gmail.com>
2017-09-20 23:05:46 +0800

    Update KillEdge.bat

--- KillEdge.bat
+++ KillEdge.bat
@@ -1,5 +1,5 @@
 REM place vbs script in shell:startup , if you are somebody as noob as justin chin or people like jeremy lai ting hong, in reality being good in video games like dota/lol doesnt make you any better than a game developer , you wont get what  what I am saying
-REM Justin Chin plagiarised my code some time ago I think, but who cares
+REM Justin Chin plagiarised my code some time ago I think, but who cares. This is open source anyway
 
 :edges
 taskkill /im MicrosoftEdge.exe
