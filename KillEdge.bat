7e944f4e56fc9f8e248644a45517203af13ff437
nwy140 <nwy140@gmail.com>
2017-09-21 14:14:10 +0800

    Update KillEdge.bat

--- KillEdge.bat
+++ KillEdge.bat
@@ -1,5 +1,4 @@
-REM place vbs script in shell:startup , if you are somebody as noob as justin chin or people like jeremy lai ting hong, you wont get what  what I am saying
-REM Justin Chin plagiarised my coding style some time ago I think, but who cares. This is open source anyway
+
 
 :edges
 taskkill /im MicrosoftEdge.exe
