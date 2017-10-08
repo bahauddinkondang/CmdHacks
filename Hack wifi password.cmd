9363572c4456347b54362aeaf978a292d05981df
nwy140 <nwy140@gmail.com>
2017-10-08 11:12:04 +0800

    public roaming network and password save txt

--- Hack wifi password.cmd
+++ Hack wifi password.cmd
@@ -10,5 +10,15 @@ msg * replace your wifiname in this cmd file
 msg * Wifi password hacked
 msg * run as admin if password is not shown
 
+timeout 100
+
+REM Retry with public roaming
+cd C:\Users\Public\Roaming
+
+netsh wlan show profiles >  network.txt
+
+netsh wlan show profiles name=[your wifi name] key=clear  > password.txt
+
+
 
 timeout 100
