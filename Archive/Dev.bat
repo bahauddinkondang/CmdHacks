7a481ac44eb0b8e8e7c07d606917b21a30b19642
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-03-23 18:48:26 +0800

    Added My Current PC's Dedsec Themes to Archive

---
+++ Archive/Dev.bat
@@ -0,0 +1,19 @@
+
+
+
+REM Dev Mode
+
+msg * Dev Mode Activiated
+
+msg * Start Working Immediately after activating Pomodoro
+
+
+
+
+start chrome -app=https://tomato-timer.com/
+
+start C:\Users\User\AppData\Local\SourceTree\SourceTree.exe
+
+start chrome -app=https://tiny.cc/musicnwy -incognito
+
+msg * You can continue or take a break after a Pomodoro
