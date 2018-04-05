863a4f866819fcc52c7d9c121f0fa89d38aba6bc
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-04-05 15:03:52 +0700

    Adding Registry files backup

--- Archive/Dev.bat
+++ Archive/Dev.bat
@@ -12,6 +12,8 @@ msg * Start Working Immediately after activating Pomodoro
 
 REM start chrome -app=https://tomato-timer.com/
 
+start chrome -app=https://www.bing.com/widget/t/timer?q=15+min+timer&height=171&pos=3&tag=afcd98f5cab196c16c439af537a0aeabbf5e064b
+
 start C:\Users\User\AppData\Local\SourceTree\SourceTree.exe
 
 start chrome -app=https://tiny.cc/musicnwy -incognito
